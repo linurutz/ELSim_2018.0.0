818e2b5facddc2ebf39bf33d133d97e75b860495
ruby <ruby@rubuntu>
2018-05-15 21:36:13 +0200

    more refactoring

--- ElSim2018/src/linear/linearResistor.cpp
+++ ElSim2018/src/linear/linearResistor.cpp
@@ -8,14 +8,29 @@
 
 #include "linearResistor.hpp"
 
-LinearResistor::LinearResistor(int elNum) : conductance(10){
+LinearResistor::LinearResistor(int elNum) : conductance(10){ // @suppress("Class members should be properly initialized")
 	this->elementNumber = elNum;
 	this->resistance = 1;
 	//TODO: implement other constructors
-	//TODO: implement setResistance
-	//TODO: getElementNumber, getConductance, getCurrent (introduce variable)
-
+	//TODO: setResistance
 
 }
 
+void LinearResistor::setResistance(double resis){
+	this->conductance *= this->resistance;
+	this->resistance = resis;
+	this->conductance /= this->resistance;
+}
 
+int LinearResistor::getElementNumber(void){
+	return this->elementNumber;
+}
+
+Matrix LinearResistor::getConductance(void){
+	return this->conductance;
+}
+
+double LinearResistor::getCurrent(Vector voltages){
+	Vector buffer = this->conductance * voltages;
+	return buffer.getValue(this->nodeTwo);
+}
