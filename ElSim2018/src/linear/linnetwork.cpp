e506d6b5a56965d1d9c8009757d5d5aae04aa79a
linurutz <linurutz@ruhh00007>
2018-05-22 18:33:34 +0200

    Update of linear network

--- ElSim2018/src/linear/linnetwork.cpp
+++ ElSim2018/src/linear/linnetwork.cpp
@@ -46,90 +46,20 @@ void LinNetwork::addVoltage(LinVoltage* voltElem){
 		this->voltages->next = 0;
 	}
 	else{
-		//First the next element and the node element are generated
-		Voltages* nextVol = new Voltages;
-		nextVol->voltagEl = voltElem;
-		nextVol->next = 0;
-		// If the element is a root element (attached to node 0) it is set to the start of the
-		// array. All followers are automatically after it.
-		if(voltElem->getPotNode() == 0){
-				// The element is turned around
-				int newRefNode = voltElem->getPotNode();
-				int newPotNode = voltElem->getRefNode();
-				double newVoltage = -voltElem->getVoltage();
-				voltElem->setPotNode(newPotNode);
-				voltElem->setRefNode(newRefNode);
-				voltElem->setVoltage(newVoltage);
+		Voltages* buffer = this->voltages;
+		while(buffer != 0){
+			buffer = buffer->next;
 		}
-		// The starting and end point are stored
-		int startNode = nextVol->voltagEl->getRefNode(); int endNode = nextVol->voltagEl->getPotNode();
-		// The voltages are stored one by one in a separate array
-		Voltages* buffer = this->voltages; Voltages* bufferStart = buffer;
-		Voltages* nextVolEnd = nextVol;
-		Voltages* volNode = buffer->next;
-		buffer->next = 0;
-		char elementFound = 0x00; // 0xXY - X inserted? Y found at start, at End in the middle
-		while(volNode->next != 0){
-			// for each element it is tested, if it is part of a branch. This is the case, when
-			// the ref node is the pot node of the last element in the buffer.
-			if(volNode->voltagEl->getRefNode() != buffer->voltagEl->getPotNode()){
-				// it must be seen if the new element is part of the current branch.
-				// There are four cases:
-				// a) The element is connected to the first node -> branch is attached to the new element
-				// b) The element is connected to the last node -> new element is attached to the branch
-				// except if the element is a zero element. in that case the branch is turned and
-				// attached to the new element
-				// c) The element is not connected: the element is attached to the end of the buffer
-				// d) The element is attached within the branch. If it is a zero element, the branch is
-				// split in two, the first part is turned around
-				if((elementFound & 0x0F) == 0x01){ // case a
-					nextVolEnd->next = bufferStart;
-					nextVolEnd = buffer;
-					elementFound |= 0x10;
-					elementFound &= 0xF0;
-				}
-				else if((buffer->voltagEl->getPotNode() == startNode) || (buffer->voltagEl->getPotNode() == endNode)){ // case b)
-					if((nextVol->voltagEl->getRefNode() == 0) || (elementFound & 0xF0)){
-
-					}
-					else{
-
-					}
-					elementFound |= 0x10;
-					elementFound &= 0xF0;
-				}
-				else if(((elementFound & 0x0F) == 0x02) && (startNode == 0)){ // case d) with zero element
-
-				}
-				else{
-
-				}
-				buffer = volNode;
-				bufferStart = buffer;
-				if(buffer->voltagEl->getRefNode() == endNode){
-					elementFound |= 0x01;
-				}
-				else if((buffer->voltagEl->getRefNode() == startNode) && (startNode != 0)){
-					if(!(elementFound & 0xF0)){
-						buffer->voltagEl->setRefNode(endNode);
-						buffer->voltagEl->setPotNode(startNode);
-						int buffnode = startNode;
-						startNode = endNode;
-						endNode = buffnode;
-						buffer->voltagEl->setVoltage(-buffer->voltagEl->getVoltage());
-
-					}
-					elementFound |= 0x01;
-				}
-			}
-			else{
-				buffer->next = volNode;
-				buffer = buffer->next;
-
-			}
-			volNode = volNode->next;
-			buffer->next = 0;
+		Voltages* newElem = new Voltages;
+		newElem->voltagEl = voltElem;
+		if(newElem->voltagEl->getPotNode() == 0){
+			int bufferNodeNum = newElem->voltagEl->getPotNode();
+			newElem->voltagEl->setPotNode(newElem->voltagEl->getRefNode());
+			newElem->voltagEl->setRefNode(bufferNodeNum);
+			newElem->voltagEl->setVoltage(-newElem->voltagEl->getVoltage());
 		}
+		newElem->next = 0;
+		buffer = newElem;
 	}
 }
 
@@ -138,9 +68,45 @@ void LinNetwork::optimizeSystem(){
 }
 
 void LinNetwork::solveSystem(){
-	// first the voltages are sorted
-
-
+	// first the Nodes have to be acquired from the resistors and the voltages
+	struct Nodearray{
+		int nodeNumber;
+		Nodearray* next;
+	};
+	Nodearray* freeNodes = 0;
+	Nodearray* voltageNodes = 0;
+	Resistors* resRun = this->resistors;
+	//Therefore the conductivity matrix is built up
+	Matrix conduct = resRun->resistorEl->getConductivity();
+	resRun = resRun->next;
+	while(resRun != 0){
+		conduct += resRun->resistorEl->getConductivity();
+		resRun = resRun->next;
+	}
+	// Now the matrix is searched for non zero elements
+	Nodearray* nodesRun = freeNodes;
+	for(int i = 0; i < conduct.getLength(); i++){
+		if(conduct.getValue(i,i) != 0){
+			if(freeNodes == 0){
+				freeNodes = new Nodearray;
+				freeNodes->nodeNumber = i;
+				freeNodes->next = 0;
+				nodesRun = freeNodes;
+			}
+			else{
+				nodesRun->next = new Nodearray;
+				nodesRun->next->nodeNumber = i;
+				nodesRun = nodesRun->next;
+				nodesRun->next = 0;
+			}
+		}
+	}
+	// Now the resistor nodes are acquired. Next the voltage nodes must be aquired
+	nodesRun = voltageNodes;
+	Voltages* buffer = this->voltages;
+	while(buffer != 0){
+		buffer = buffer->next;
+	}
 }
 
 Vector LinNetwork::getNodalVoltages(){
